8229c361d3d5c3b238c4a84c1a34d6e732a259fd
Priscila <priscilayaeko@icloud.com>
2025-05-14 20:50:27 -0300

    Teste_projeto_simples

---
+++ src/main/java/projeto_teste/App.java
@@ -0,0 +1,13 @@
+package projeto_teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/projeto_teste/LoginTeste.java
@@ -0,0 +1,28 @@
+package projeto_teste;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LoginTeste {
+
+    @Test
+    @DisplayName("Fazer Login na aplicação")
+    public void fazerLogin() {
+        WebDriver driver = new ChromeDriver(); // Exemplo com ChromeDriver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://applications.fsbr.com.br/homolog/assine--frontend/");
+
+        driver.findElement(By.cssSelector("#login")).sendKeys("priscila.cunha@fsbr.com.br");
+        driver.findElement(By.cssSelector("#senha")).sendKeys("Billy@2436");
+        driver.findElement(By.xpath("//button[normalize-space()='Entrar']")).click();
+
+        // Fechar o navegador após o teste (opcional, mas recomendado)
+        driver.quit();
+    }
+}
