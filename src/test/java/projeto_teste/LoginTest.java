475fb30e45d7ec300000565c7e76f2f41ebd54c2
Priscila Cunha <pri_mc4@hotmail.com>
2025-05-15 10:16:31 -0300

    Ajuste do código para rodar o test LoginTest

--- src/test/java/projeto_teste/LoginTest.java
+++ src/test/java/projeto_teste/LoginTest.java
@@ -5,15 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
-public class LoginTeste {
+public class LoginTest {
 
     @Test
     @DisplayName("Fazer Login na aplicação")
     public void fazerLogin() {
-        WebDriver driver = new ChromeDriver(); // Exemplo com ChromeDriver
+
+       // Configuração do Chrome para rodar em ambiente CI (como GitHub Actions)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Executa sem interface gráfica
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://applications.fsbr.com.br/homolog/assine--frontend/");
@@ -21,8 +28,12 @@ public class LoginTeste {
 
         driver.findElement(By.xpath("//body/header/div[@class='buttons-header']/a[1]")).click();
 
-        driver.findElement(By.cssSelector("#login")).sendKeys("priscila.cunha@fsbr.com.br");
-        driver.findElement(By.cssSelector("#senha")).sendKeys("Billy@2436");
+        String email = System.getenv("LOGIN_EMAIL");
+        String senha = System.getenv("LOGIN_SENHA");
+        
+
+        driver.findElement(By.cssSelector("#login")).sendKeys(email);
+        driver.findElement(By.cssSelector("#senha")).sendKeys(senha);
         driver.findElement(By.xpath("//button[normalize-space()='Entrar']")).click();
 
         // Fechar o navegador após o teste (opcional, mas recomendado)
