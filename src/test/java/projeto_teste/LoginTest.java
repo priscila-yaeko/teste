346cd469bb2e2f329aeff54c5f7f4672cf69ea73
Priscila Cunha <pri_mc4@hotmail.com>
2025-05-15 15:13:13 -0300

    Ajuste do deploy allure roport

--- src/test/java/projeto_teste/LoginTest.java
+++ src/test/java/projeto_teste/LoginTest.java
@@ -1,66 +1,28 @@
 package projeto_teste;
 
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Step;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
-
 import java.time.Duration;
 
-@Owner("Priscila Yaeko Cunha")
+@Epic("Login")
+@Feature("Tela de Login")
 public class LoginTest {
 
     @Test
+    @Story("Login com credenciais válidas")
     @DisplayName("Fazer Login na aplicação")
-    @Description("Testa o login na aplicação com usuário válido e senha válida.")
+    @Description("Teste que faz login com email e senha vindos do arquivo .env ou variáveis de ambiente")
     @Severity(SeverityLevel.CRITICAL)
     public void fazerLogin() {
-        WebDriver driver = setupDriver();
 
-        try {
-            abrirPagina(driver);
-            clicarBotaoLogin(driver);
-            preencherCredenciais(driver);
-            clicarBotaoEntrar(driver);
-        } finally {
-            driver.quit();
-        }
-    }
-
-    @Step("Configurar WebDriver Chrome headless")
-    private WebDriver setupDriver() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        return driver;
-    }
-
-    @Step("Abrir página inicial")
-    private void abrirPagina(WebDriver driver) {
-        driver.get("https://applications.fsbr.com.br/homolog/assine--frontend/");
-    }
-
-    @Step("Clicar no botão login")
-    private void clicarBotaoLogin(WebDriver driver) {
-        driver.findElement(By.xpath("//body/header/div[@class='buttons-header']/a[1]")).click();
-    }
-
-    @Step("Preencher as credenciais de login")
-    private void preencherCredenciais(WebDriver driver) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
         String email = System.getenv("LOGIN_EMAIL");
@@ -77,12 +39,25 @@ public class LoginTest {
             throw new IllegalStateException("LOGIN_EMAIL e LOGIN_SENHA não definidos");
         }
 
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://applications.fsbr.com.br/homolog/assine--frontend/");
+
+        driver.findElement(By.xpath("//body/header/div[@class='buttons-header']/a[1]")).click();
         driver.findElement(By.cssSelector("#login")).sendKeys(email);
         driver.findElement(By.cssSelector("#senha")).sendKeys(senha);
-    }
-
-    @Step("Clicar no botão entrar")
-    private void clicarBotaoEntrar(WebDriver driver) {
         driver.findElement(By.xpath("//button[normalize-space()='Entrar']")).click();
+
+        driver.quit();
     }
 }
